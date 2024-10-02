5352d10f604a9e827f15bc4924ae30d240c10324
quyen <naundylan@gmail.com>
2024-10-02 09:53:26 +0700

    update week2 exercise

--- Week 2/3.10.cpp
+++ Week 2/3.10.cpp
@@ -1,50 +1,75 @@
-#include<bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-typedef long long ll;
-
-ll gcd(ll a, ll b){
-	if( b == 0 ) return a;
-	return gcd(b, a % b );
-}
-
-class PhanSo{
+class phanso {
 	private:
-		ll tu, mau;
+		int t, m;
 	public:
-		PhanSo(ll tu, ll mau);
-		friend ostream& operator << (ostream&, PhanSo);
-		friend istream& operator >> (istream&, PhanSo&);
+		void nhap();
+		void xuat();
+		phanso cong(phanso p2);
+		phanso tich(phanso p2);
 		void rutgon();
 };
 
 
-PhanSo::PhanSo(ll tu , ll mau){
-	this->tu = tu;
-	this->mau = mau;
-	
+void phanso::nhap() {
+	cout<<"Nhap phan tu: ";
+	cin>>t;
+	do {
+		cout<<"Nhap phan mau khac 0: ";
+		cin>>m;
+	} while(m == 0);
 }
 
-ostream& operator << (ostream& out, PhanSo a){
-	out << a.tu << "/" << a.mau << endl;
-	return out; 
-}
-istream& operator >> (istream& in, PhanSo &a){
-	in >> a.tu >> a.mau;
-	return in;
+void phanso::xuat() {
+	cout<<"Phan so: "<<t<<" "<<m<<endl;
 }
 
-void PhanSo::rutgon(){
-	ll g = gcd(tu,mau);
-	tu /= g;
-	mau /= g;
+int gcd(int a, int b) {
+	while(b!=0) {
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
 }
 
-int main(){
-	PhanSo p(1,1);
-	cin>>p;
+void phanso::rutgon() {
+	int ucln = gcd(t, m);
+	t /= ucln;
+	m /= ucln;
+}
+
+phanso phanso::cong(phanso p2) {
+	phanso p;
+	p.t = t*p2.m + m*p2.t;
+	p.m = m * p.m;
 	p.rutgon();
-	cout<<p;
+	return p;
+}
+
+phanso phanso::tich(phanso p2) {
+	phanso p;
+	p.t = t * p2.t;
+	p.m = m * p2.m;
+	p.rutgon();
+	return p;
+}
+
+int main() {
+	phanso a, b;
+	a.nhap();
+	b.nhap();
+	
+	a.xuat();
+	b.xuat();
+	
+	phanso tich, tong;
+	tich = a.tich(b);
+	tong = a.cong(b);
+	
+	tich.xuat();
+	tong.xuat();
 	return 0;
 }
