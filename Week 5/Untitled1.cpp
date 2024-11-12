2169155fc02f9be2ccecd90df506605becb8f759
quyen <naundylan@gmail.com>
2024-11-12 07:48:19 +0700

    update week5 examples

---
+++ Week 5/Untitled1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ussing namespace std;
+
+// ke thua theo kieu public thi public A thanh public B, protected A thanh protected B
+// ke thua theo kieu protected thi public va protected cua A deu tro thanh protected cua B
+// ke thua theo kieu private thi public va protected cua A se tro thanh private cua B
+
+class A {
+	private:
+	protected:
+	public:
+};
+
+class B mode A{
+	private:
+	protected:
+	public:
+};
+
+int main() {
+	
+	return 0;
+}
